b6261cf7c72166a9f3f027e704d6ca45449687f5
DARKEH ELIJAH <elijahagbedam@gmail.com>
2021-12-14 17:27:18 +0000

    changes to express,todo,todoController

---
+++ controllers/todoController.js
@@ -0,0 +1,26 @@
+const todo = require('../models/todos.js');
+
+function addTodo() {
+    
+}
+
+function updateTodoById() {
+
+}
+
+function deleteTodoById() {
+
+
+}
+
+function getTodoById() {
+
+}
+
+function getAllTodos() {
+
+}
+
+module.exports ={
+    addTodo,updateTodoById,deleteTodoById,getTodoById,getAllTodos
+}
--- express_app.js
+++ express_app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const server = express();
+const todoController = require('./controllers/todoController');
 const mongo_db_url =
-  "mongodb+srv://Darkeh1807:alisajunior@cluster0.guusu.mongodb.net/tood_db ?retryWrites=true&w=majority";
-server.get('/task', function (request,respond) {
-    respond.status(200).json('Tasks has been given');
-});
-server.post("/task", function (request, respond) {
-    respond.status(200).json('All are new task');
-});
-server.put( "task/",function (request,respond) {
-      respond.status(200).json("Update student");
-});
-server.delete("task", function (request,respond) {
-      respond.status(200).json("Delete student");
-});
+  "mongodb+srv://Darkeh1807:alisajunior@cluster0.guusu.mongodb.net/tood_db?retryWrites=true&w=majority";
+
+server.get('/todos',todoController.getAllTodos);
+server.post("todos/",todoController.addTodo );
+server.put( "todos/",todoController.updateTodoById);
+server.delete("todos",todoController.deleteTodoById );
+
 server.listen(4000, function () {
     console.log('Server is running on express');
+    mongoose.connect(mongo_db_url).then(
+        function () {
+            console.log('DB is connected');
+        }
+    ).
+    catch(
+       function (error) {
+           console.log('DB is not connected:', error.message);
+       }
+    );
 })
 
